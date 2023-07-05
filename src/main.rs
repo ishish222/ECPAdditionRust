cc8258a0c22875556829167b8d2b60a2dd25592e
ishish.io <ts@ishish.io>
2023-07-05 19:50:15 +0200

    Attempted to implement addition relying on Tonelli-Shanks algorithm for finding square roots
    Article: https://medium.com/asecuritysite-when-bob-met-alice/adding-points-in-elliptic-curve-cryptography-a1f0a1bce638
    
    There are some type errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::env;
 use std::fmt::{
     Display, 
@@ -6,6 +7,7 @@ use std::fmt::{
     Result as FmtResult, Error
 };
 use std::ops::Add;
+
 
 fn main() {
     let args = env::args();
@@ -21,12 +23,13 @@ fn main() {
 
     dbg!(&my_ec);
 
-    let point_p = EllipticCurvePoint::new(1, 2, &my_ec);
-    let point_q = EllipticCurvePoint::new(3, 4, &my_ec);
+    let point_p = EllipticCurvePoint::new(6, 1, &my_ec);
+    let point_q = EllipticCurvePoint::new(8, 1, &my_ec);
 
     dbg!(&point_p);
     dbg!(&point_q);
 
+/* 
     println!("1^2 % p = {}", my_ec.pow(1, 2));
     println!("2^2 % p = {}", my_ec.pow(2, 2));
     println!("3^2 % p = {}", my_ec.pow(3, 2));
@@ -42,10 +45,10 @@ fn main() {
     println!("ls(8) % p = {}", my_ec.legendre_symbol(8));
     println!("ls(9) % p = {}", my_ec.legendre_symbol(9));
     println!("ls(10) % p = {}", my_ec.legendre_symbol(10));
+*/
+    let point_r = point_p + point_q;
 
-    //let point_r = point_p + point_q;
-
-    //dbg!(&point_r);
+    dbg!(&point_r);
 }
 
 struct EllipticCurve {
@@ -59,8 +62,49 @@ impl EllipticCurve {
         Self { a, b, p }
     }
 
-    fn modular_sqrt(x: u64) -> u64 {
-        unimplemented!();
+    fn modular_sqrt(&self, a: u64) -> u64 {
+        let p = self.p;
+
+        if self.legendre_symbol(a) != 1 { return 0; }
+        else if a == 0 { return 0; }
+        else if p == 2 { return p; }
+        else if p % 4 == 3 { return self.pow(a, (p+1)/4); }
+
+        let mut s = p - 1;
+        let mut e = 0;
+
+        while s % 2 == 0 {
+            s /= 2;
+            e += 1;
+        }
+
+        let mut n = 2;
+        while self.legendre_symbol(n) != -1 { n += 1; }
+
+        let mut x = self.pow(a, (s + 1) / 2);
+        let mut b = self.pow(a, s);
+        let mut g = self.pow(n, s);
+        let mut r = e;
+
+        loop {
+            let mut t = b;
+            let mut m = 0;
+
+            for m in 0..r {
+                if t == 1 {
+                    break;
+                }
+                t = self.pow(t, 2);
+            }
+
+            if m == 0 { return x; }
+
+            let gs = self.pow(g, self.pow(2, r - m - 1));
+            g = (gs * gs) % p;
+            x = (x * gs) % p;
+            b = (b * g) % p;
+            r = m;
+        }
     }
 
     fn legendre_symbol(&self, a: u64) -> i8 {
@@ -76,6 +120,27 @@ impl EllipticCurve {
                 Ok(r) => {return r}
                 Err(_) => { dbg!("Error while calculating legendre_symbol"); return 0;}
             }
+        }
+    }
+
+    fn egcd(a: u64, b: u64) -> (u64, u64, u64) {
+        if a == 0 {
+            return (b, 0, 1);
+        }
+        else {
+            let (g, y, x) = EllipticCurve::egcd(b % a, a);
+            return (g, x - (b / a) * y, y);
+        }
+    }
+
+    fn modinv(&self, a: u64) -> u64 {
+        let (g, x, y) = EllipticCurve::egcd(a, self.p);
+        if g != 1 {
+            println!("Point x is not on the curve, please select another");
+            panic!();
+        }
+        else {
+            return x % self.p;
         }
     }
 
@@ -143,11 +208,24 @@ impl<'ec> Add for EllipticCurvePoint<'ec> {
         let grad_x = rhs.x - self.x;
         let grad = grad_y / grad_x;
 
-        unimplemented!();
+        let a = self.ec.a;
+        let b = self.ec.b;
+        let p = self.ec.p;
+
+        let z=(self.ec.pow(self.x, 3) + a * self.x + b) % p;
+        let y1=self.ec.modular_sqrt(z);
+
+        let z=(self.ec.pow(rhs.x, 3) + a * rhs.x + b) % p;
+        let y2=self.ec.modular_sqrt(z);
+
+        let s=(y2-y1)* self.ec.modinv(self.x-rhs.x);
+
+        let x3 = (self.ec.pow(s, 2)-self.x-rhs.x) % p;
+        let y3 = (s*(rhs.x-x3)-y2) % p;
 
         Self {
-            x: 0,
-            y: 0,
+            x: x3,
+            y: y3,
             ec: self.ec,
         }
     }
